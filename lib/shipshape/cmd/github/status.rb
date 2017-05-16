fb3d26985c7e8d1b24f3da694ba5ae738bc1bd84
Ian MacDonald <imacdonald@rumblefish.com>
2017-05-15 19:37:54 -0700

    Start adding status command

--- lib/shipshape/cmd/github/status.rb
+++ lib/shipshape/cmd/github/status.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'thor'
+require 'octokit'
 
 module Shipshape
   class Github < Thor
@@ -9,20 +10,36 @@ module Shipshape
 
       desc 'post STATE CONTEXT DESCRIPTION',
            'Update github repo commit status'
+      option :owner,
+             aliases: %(-O --O),
+             desc: 'Github repository owner'
       option :repo,
              default: Pathname.pwd.basename.to_s,
              aliases: %(-R --R),
              desc: 'Github repository name'
-      option :build_url,
-             aliases: %(-U --U --build-url --build-uri),
+      option :target_url,
+             aliases: %(-U --U --target-url --target-uri),
              desc: 'Link to source of status'
-      option :github_access_token,
-             default: ENV['BUNDLE_GITHUB__COM'],
+      option :access_token,
+             default: ENV['BUNDLE_GITHUB__COM'].split(':').first,
              aliases: %(-T --T --access-token),
              desc: 'OAuth token for access to Github'
       def post(state, context, description)
-        git_head = run('git rev-parse --verify HEAD')
+        git_sha = run('git rev-parse --verify HEAD', capture: true).chomp
+
+        resp = client.create_status(
+          options[:repo], git_sha, state,
+          context: context,
+          description: description,
+          target_url: options[:target_url]
+        )
         binding.pry
+      end
+
+      private
+
+      def client
+        @client ||= Octokit::Client.new(access_token: options[:access_token])
       end
     end
 
